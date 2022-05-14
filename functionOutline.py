b4a583354ade77e042d3a26c7f7df68f9bd88b7d
sbloch3 <93631652+sbloch3@users.noreply.github.com>
2022-05-14 12:12:45 -0500

    Add files via upload

---
+++ functionOutline.py
@@ -0,0 +1,41 @@
+'''
+This outline will help solidify concepts from the Functions lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1) Make a function that has two boolean parameters. If both booleans are
+#true, return true. Else, return false. Then, call the function.
+#Print what the function returns.
+def result(a and b):
+    if a and b
+        return True
+    else
+        return False
+
+
+
+#2) Make a function that takes one int parameter: gallons. Convert gallons
+#to cups (do this by multiplying gallons by 16). Then return cups. Then,
+#call the function.
+#Print what the function returns.
+def gallons(cups):
+    for x in cups:
+        print(x)
+        return 
+    
+    gallons = x * 16
+    print(gallons(cups))
+
+
+
+#3) Make a function of any any kind with a common SYNTAX error. Then, call the
+#function.
+#Print what the function returns.
+def gallons(cups)
+    for x in cups:
+        print(x)
+        return 
+    
+    gallons = x * 16
+    print(gallons(cups))
+
